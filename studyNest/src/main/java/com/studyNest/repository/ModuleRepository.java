ce5919c70c59eed4636271fb13e36e78259fc04b
deepakdey412@gmail.com <deepakdey412@gmail.com>
2025-09-14 17:57:15 +0530

    Remove project folder

--- studyNest/src/main/java/com/studyNest/repository/ModuleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.studyNest.repository;
-
-import com.studyNest.model.Module;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ModuleRepository extends JpaRepository<Module, Long> {
-    List<Module> findBySemester(int semester);
-}
