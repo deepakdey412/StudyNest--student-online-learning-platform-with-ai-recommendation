51c379d85c4f2e40213f15e647b46bfb20b47e17
deepakdey412@gmail.com <deepakdey412@gmail.com>
2025-09-15 16:52:01 +0530

    Update Repository

--- Study-Nest/src/main/java/com/deepak/Study_Nest/dao/StudentDetailsRepository.java
+++ Study-Nest/src/main/java/com/deepak/Study_Nest/dao/StudentDetailsRepository.java
@@ -3,5 +3,10 @@ package com.deepak.Study_Nest.dao;
 import com.deepak.Study_Nest.entity.StudentDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StudentDetailsRepository extends JpaRepository<StudentDetails , Long> {
+
+    Optional<StudentDetails> findByEmail(String email);
+
 }
