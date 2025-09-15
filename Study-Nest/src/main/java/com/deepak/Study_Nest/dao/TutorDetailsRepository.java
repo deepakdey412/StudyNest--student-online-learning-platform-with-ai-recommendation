601b3446f7b825dc098a77555d2f5f8862be6d1f
deepakdey412@gmail.com <deepakdey412@gmail.com>
2025-09-15 16:54:18 +0530

    Update Student service

--- Study-Nest/src/main/java/com/deepak/Study_Nest/dao/TutorDetailsRepository.java
+++ Study-Nest/src/main/java/com/deepak/Study_Nest/dao/TutorDetailsRepository.java
@@ -3,5 +3,8 @@ package com.deepak.Study_Nest.dao;
 import com.deepak.Study_Nest.entity.TutorDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TutorDetailsRepository extends JpaRepository<TutorDetails, Long> {
+    Optional<TutorDetails> findByEmail(String email);
 }
--- Study-Nest/src/main/java/com/deepak/Study_Nest/service/StudentDetailsService.java
+++ Study-Nest/src/main/java/com/deepak/Study_Nest/service/StudentDetailsService.java
@@ -2,29 +2,34 @@ package com.deepak.Study_Nest.service;
 
 import com.deepak.Study_Nest.dao.StudentDetailsRepository;
 import com.deepak.Study_Nest.entity.StudentDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class StudentDetailsService  {
+public class StudentDetailsService {
 
     private final StudentDetailsRepository studentDetailsRepo;
+    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public StudentDetailsService(StudentDetailsRepository studentDetailsRepo) {
         this.studentDetailsRepo = studentDetailsRepo;
     }
-//Add Student
-    public  void addStudent(StudentDetails studentDetails) {
+
+    //Add Student
+    public void addStudent(StudentDetails studentDetails) {
+        studentDetails.setStudentPassword(passwordEncoder.encode(studentDetails.getStudentPassword()));
         studentDetailsRepo.save(studentDetails);
     }
-    //find Student by id
-    public  StudentDetails addStudent(Long studentID) {
-        return studentDetailsRepo.findAllById(studentID);
-    }
-
-
-    public  void addStudent(StudentDetails studentDetails) {
-        studentDetailsRepo.save(studentDetails);
-    }
+    
+//    //find Student by id
+//    public  StudentDetails addStudent(Long studentID) {
+//        return studentDetailsRepo.findAllById(studentID);
+//    }
+//
+//
+//    public  void addStudent(StudentDetails studentDetails) {
+//        studentDetailsRepo.save(studentDetails);
+//    }
 
 
 }
